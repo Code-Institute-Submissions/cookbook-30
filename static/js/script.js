e5bb65a49566ad697b4422939b21c6195fd2bca5
sophnagle <sophnagle@gmail.com>
2021-03-21 14:32:44 +0000

    Create add_reciple template and add recipe form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.chips').chips();
+        $('.chips-placeholder').chips({
+                placeholder: 'Enter recipe tags',
+                secondaryPlaceholder: '+Tag',
+        });
+     
 });
