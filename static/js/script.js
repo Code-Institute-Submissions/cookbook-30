55546dd798937f38bc573d65263fc649b4344823
sophnagle <sophnagle@gmail.com>
2021-04-22 16:47:26 +0000

    Add comment explanations to code

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
+    /* Side navigation opens to the right */
     $('.sidenav').sidenav({edge: "right"});
+    /* Materialize's Tag jQuery to create custom and placeholder */
     $('.chips').chips();
     $('.chips-placeholder').chips({
             placeholder: 'Enter recipe tags',
             secondaryPlaceholder: '+ Tag'
         });
-    $('.slider').slider();
+    /* Scroll effect on items with class 'fadeIn' */
     $(window).scroll(function () {
         $(".fadeIn").each(function (i) {
         $(this)
